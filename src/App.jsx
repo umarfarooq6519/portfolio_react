def3a6162b93eb0217f5b593fbca1bf91d3ea466
Umar Farooq <149324609+umarfarooq6519@users.noreply.github.com>
2024-07-05 17:35:40 +0500

    change loading code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import AboutPage from "./components/AboutPage";
 import ContactPage from "./components/ContactPage";
 import ExpertisePage from "./components/ExpertisePage";
@@ -5,46 +6,33 @@ import HomePage from "./components/HomePage";
 import Loading from "./components/Loading";
 import ProjectsPage from "./components/ProjectsPage";
 
-import { useState, useEffect } from "react";
-
 function App() {
   const [loading, setLoading] = useState(true);
-  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
-    // Simulate a data fetch
-    setTimeout(() => {
+    const loadData = async () => {
+      // Simulate a loading process
+      await new Promise((resolve) => setTimeout(resolve, 3000));
       setLoading(false);
-    }, 3000);
-  }, []);
+    };
 
-  useEffect(() => {
-    // Simulate a data fetch
-    setTimeout(() => {
-      setLoading(false);
-      // Delay the visibility change to allow for the loader to disappear
-      setTimeout(() => {
-        setVisible(true);
-      }, 60); // Adjust the delay if needed
-    }, 3000); // Adjust the timeout duration as needed
+    loadData();
   }, []);
-
-  if (loading) {
-    return <Loading />;
-  }
 
   return (
-    <section
-      className={`App text-base transition-all fade-in ${
-        visible ? "visible" : ""
-      }`}
-    >
-      <HomePage />
-      <AboutPage />
-      <ExpertisePage />
-      <ProjectsPage />
-      <ContactPage />
-      <Footer />
+    <section className="App text-base">
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className={`animate-fadeIn`}>
+          <HomePage />
+          <AboutPage />
+          <ExpertisePage />
+          <ProjectsPage />
+          <ContactPage />
+          <Footer />
+        </div>
+      )}
     </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.7s ease-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
   },
   daisyui: {
     themes: ["light", "dark", "cupcake"],
